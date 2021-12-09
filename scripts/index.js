5f1c3ad5894dd98f19eceb61f9d7236917a98dcb
hieudeptrai1962000 <66685799+hieudeptrai1962000@users.noreply.github.com>
2021-12-09 15:49:26 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Oct 18, 2021',
+  birthdate: 'Oct 18, 2022',
   name: 'Bạn NGỌC'
 
 };
